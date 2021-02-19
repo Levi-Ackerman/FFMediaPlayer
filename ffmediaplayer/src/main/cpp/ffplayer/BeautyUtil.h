8cd6743959d0c5d8a9313460730c1fe88d659ee3
lizhengxian.ackerman <lizhengxian.ackerman@bytedance.com>
2021-02-19 11:17:31 +0800

    并行+查表

--- ffmediaplayer/src/main/cpp/ffplayer/BeautyUtil.h
+++ ffmediaplayer/src/main/cpp/ffplayer/BeautyUtil.h
@@ -8,6 +8,11 @@
 #include <WhiteTable.h>
 #include <chrono>
 #include <TimeClock.h>
+#include <thread>
+#include <future>
+
+static const int THREAD_NUM = 4;
+static std::future<void> futs[THREAD_NUM];
 
 extern "C" {
 void skinWhite(uint8_t *input, uint8_t *output, int width, int height, int channels) {
@@ -15,11 +20,22 @@ void skinWhite(uint8_t *input, uint8_t *output, int width, int height, int chann
     return;
   }
   TimerClock clock;
-  clock.update();
   const int size = width * height * channels;
-  for (int i = 0; i < size; ++i) {
-    output[i] = WhiteTable::getWhitePixelValue(input[i]);
+  const int section_length = size / THREAD_NUM;
+  for (int i = 0; i < THREAD_NUM; ++i) {
+    futs[i] =
+        std::move(std::async(std::launch::async, [](int i, int size, uint8_t *input, uint8_t *output,int section_length) {
+          int start = i * section_length;
+          int end = min(i * section_length + section_length, size);
+          for (int j = start; j < end; ++j) {
+            output[j] = WhiteTable::getWhitePixelValue(input[j]);
+          }
+        }, i, size, input, output,section_length));
   }
+  for (int i = 0; i < THREAD_NUM; ++i) {
+    futs[i].get();
+  }
+
   auto t = clock.getTimerMilliSec();
   ALOGI("time cost :%lf", t);
 }
